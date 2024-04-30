7865daaf76aba6de8542e6d7a6744d095cbca437
YashSingh25 <hustleryash25@gmail.com>
2024-04-30 19:17:15 +0530

    Student Class Added.

---
+++ Student.java
@@ -0,0 +1,10 @@
+
+public class Student {
+    public Student(){
+        System.out.println("Student Object Created.");
+    }
+
+    public void sayHello(){
+        System.out.println("Hey, I am Student!");
+    }
+}
